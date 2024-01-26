528fe585ad060e97307ecafcf0844452142d4e2b
aneesazc <41589553+aneesazc@users.noreply.github.com>
2024-01-26 23:40:07 +0530

    Update validAnagram.py

--- Array & Hashing/validAnagram.py
+++ Array & Hashing/validAnagram.py
@@ -16,3 +16,21 @@ class Solution:
                 hashMap2[c] += 1
 
         return hashMap1 == hashMap2
+
+
+
+class Solution2:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        
+        return True
