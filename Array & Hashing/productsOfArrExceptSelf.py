5003120b6cd2557a85e24693b461929b234f5904
aneesazc <41589553+aneesazc@users.noreply.github.com>
2024-01-30 11:48:09 +0530

    Create productsOfArrExceptSelf.py

---
+++ Array & Hashing/productsOfArrExceptSelf.py
@@ -0,0 +1,22 @@
+# Time, Space - O(n), O(1)
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+            answer = [1] * len(nums)
+            prefix = 1
+            postfix = 1
+
+            for i in range(len(nums)):
+                answer[i] = prefix
+                prefix = prefix * nums[i]
+                
+            for i in range(len(nums) - 1, -1, -1):
+                answer[i] *= postfix
+                postfix = postfix * nums[i]
+                
+            return answer
+
+# Input-   [1,   2,  3, 4]
+# prefix-  [1,   1,  2, 6]
+# postfix- [24, 24, 12, 4]
+# output-  [24, 12,  8, 6]
