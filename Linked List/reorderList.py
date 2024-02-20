bdd55215dbc377ebff61a3c024078a3f0180a757
aneesazc <41589553+aneesazc@users.noreply.github.com>
2024-02-20 15:49:58 +0530

    Create reorderList.py

---
+++ Linked List/reorderList.py
@@ -0,0 +1,39 @@
+# Time, Space- O(n), O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # find the middle
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        second = slow.next
+        slow.next = None # used to signify the end of the first half
+        prev = None 
+
+        # reverse the second half
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+
+        first, second = head, prev
+
+        # merge the two halfs
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first = temp1
+            second = temp2
+        
