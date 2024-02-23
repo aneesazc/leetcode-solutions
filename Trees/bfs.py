8150e143e8c2807dbcf0315b08d12fc1c409f7bb
aneesazc <41589553+aneesazc@users.noreply.github.com>
2024-02-23 20:19:52 +0530

    Create bfs.py

---
+++ Trees/bfs.py
@@ -0,0 +1,52 @@
+# Time- O(n)
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def bfs(root):
+    queue = deque()
+
+    if root:
+        queue.append(root)
+    
+    level = 0
+    while len(queue) > 0:
+        print("level: ", level)
+        for i in range(len(queue)):
+            curr = queue.popleft()
+            print(curr.val)
+            if curr.left:
+                queue.append(curr.left)
+            if curr.right:
+                queue.append(curr.right)
+        level += 1
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        queue = deque()
+        res = []
+
+        if root:
+            queue.append(root)
+
+        while len(queue) > 0:
+            level = []
+            for i in range(len(queue)):
+                curr = queue.popleft()
+                level.append(curr.val)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+            res.append(level)
+
+        return res
+
+
+
+
