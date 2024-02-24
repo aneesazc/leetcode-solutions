ede60239be9f0ad46093254ed3f0ad2bf9fcb148
aneesazc <41589553+aneesazc@users.noreply.github.com>
2024-02-24 12:28:38 +0530

    Create invertTree.py

---
+++ Trees/invertTree.py
@@ -0,0 +1,50 @@
+# For this problem children nodes get swapped but so do their children and so on...
+# Hence this is a recursive problem and we can use DFS for this- either preorder or postorder
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        # swap the 2 children
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+        
+        return root
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        # swap the children
+        root.left, root.right = root.right, root.left
+        
+        # make 2 recursive calls
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
