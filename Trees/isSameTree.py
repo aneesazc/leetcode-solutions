b11e7f1b6fc4bbc5320b6b11933cd7d893c4e27d
aneesazc <41589553+aneesazc@users.noreply.github.com>
2024-02-25 19:44:42 +0530

    Create isSameTree.py

---
+++ Trees/isSameTree.py
@@ -0,0 +1,26 @@
+# O(p + q) time
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # If both nodes are None, they are the same.
+        if not p and not q:
+            return True
+        # If one is None and the other is not, they aren't the same.
+        if not p or not q:
+            return False
+        # If the values of the nodes are different, they aren't the same.
+        if p.val != q.val:
+            return False
+        
+        # Recursively check the left and right children.
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
