cc9c969d20dfce98644de9b59c9be9d13dad0f2f
aneesazc <41589553+aneesazc@users.noreply.github.com>
2024-02-26 15:54:59 +0530

    Update isSameTree.py

--- Trees/isSameTree.py
+++ Trees/isSameTree.py
@@ -17,6 +17,19 @@ class Solution:
 
 
 class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True # empty trees are technically equal
+
+        if not p or not q or p.val != q.val:
+            return False # if either trees are null or have different values means NOT equal
+
+        # recursively check if the left subtrees and right subtrees are equal
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+
+class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         if not p and not q:
             return True
