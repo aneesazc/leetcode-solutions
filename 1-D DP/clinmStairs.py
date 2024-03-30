82f7d3606a0bf52e0ce47147c29804b0b26dc315
aneesazc <41589553+aneesazc@users.noreply.github.com>
2024-03-31 00:24:31 +0530

    Update clinmStairs.py

--- 1-D DP/clinmStairs.py
+++ 1-D DP/clinmStairs.py
@@ -45,4 +45,15 @@ class Solution:
             i -= 1
 
         return arr[0]
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1
+        for i in range(n - 2, -1, -1):
+            tmp = one
+            one = one + two
+            two = tmp
+
+        return one
       
