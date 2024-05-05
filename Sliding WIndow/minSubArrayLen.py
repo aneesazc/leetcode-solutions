e3e3549ea1f1345a867c1a61fec2134bffe5fe47
aneesazc <41589553+aneesazc@users.noreply.github.com>
2024-05-05 09:05:41 +0530

    Create minSubArrayLen.py

---
+++ Sliding WIndow/minSubArrayLen.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        res = float("inf")
+        L = 0
+        curSum = 0
+
+        for R in range(len(nums)):
+            curSum += nums[R]
+            while curSum >= target:
+                # Calculate the length of the current valid window
+                res = min(res, R - L + 1)
+                # Subtract the left-most value and move L to the right to see if we can find a smaller window
+                curSum -= nums[L]
+                L += 1
+
+        return res if res != float('inf') else 0
